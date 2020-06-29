6dcc51f9b9ed661f432a1080b8432cccef6189ac
aparappi <aparappi@iu.edu>
2020-06-29 00:07:59 -0400

    multi ques convo

--- index.js
+++ index.js
@@ -98,46 +98,49 @@ controller.hears(['bamboo access', 'hi'], 'direct_message', function (bot, messa
     });*/
 
    bot.startConversation(message, function(err, convo) {
+convo.addMessage('This is the end!', 'new');
 
-    // create a path for when a user says YES
-    convo.addMessage({
-            text: 'You said yes! How wonderful.',
-    },'yes_thread');
-
-    // create a path for when a user says NO
-    convo.addMessage({
-        text: 'You said no, that is too bad.',
-    },'no_thread');
-
-    // create a path where neither option was matched
-    // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
-    convo.addMessage({
-        text: 'Sorry I did not understand.',
-        action: 'default',
-    },'bad_response');
-
-    // Create a yes/no question in the default thread...
-    convo.addQuestion('Do you like cheese?', [
+    convo.addQuestion('what to do now?', [
         {
             pattern: 'yes',
-            callback: function(response, convo) {
-                convo.gotoThread('yes_thread');
-            },
+            callback: function(response,convo) {
+              convo.say('OK you are done!');
+              convo.next();
+            }
+        }, 
+        {
+            pattern: 'noo',
+            callback: function(response,convo) {
+                console.log("==>", response)
+                convo.addQuestion('why not?', [
+                        {
+                            pattern:'idk', 
+                            callback: function(response,convo) {
+                                    convo.say('doofus');
+                                    convo.next();
+                            } 
+                }, 
+            {
+                                pattern:'bcoz', 
+                            callback: function(response,convo) {
+                                    convo.say('kaay');
+                                    convo.next();
+                            } 
+            }
+                    ])
+              convo.next();
+            }
         },
         {
-            pattern: 'no',
-            callback: function(response, convo) {
-                convo.gotoThread('no_thread');
-            },
-        },
-        {
-            default: true,
-            callback: function(response, convo) {
-                convo.gotoThread('bad_response');
-            },
+            pattern: 'go',
+            callback: function(response,convo) {
+             convo.transitionTo("new", "going")
+              convo.next();
+            }
         }
-    ],{},'default');
 
+        ])
+   
     convo.activate();
 });
 
